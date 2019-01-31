dd6897a37e1f3da32bb5ab0e30e016131d158a0a
RickonChen <2298847021@qq.com>
2019-01-31 16:41:24 +0800

    update home page

--- project/base/views.py
+++ project/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 
 def home(request):
-    return render(request, 'base/home.html')
+    items = Post.objects.all()
+    return render(request, 'base/home.html', { 'items': items, 'title': 'Home' })
--- project/blog/views.py
+++ project/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from blog.models import Post
 
 def post(request, slug=None):
     item = get_object_or_404(Post, slug=slug)
