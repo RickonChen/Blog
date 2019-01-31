24f477f6703f30071469de46b3946822531f44a4
RickonChen <2298847021@qq.com>
2019-01-31 10:01:50 +0800

    add post page

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post(request, slug=None):
+    item = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/post.html', {'item': item, 'title': item,})
