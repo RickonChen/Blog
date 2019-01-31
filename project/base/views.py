b53ff5c4f7170138731b0782b10f74acf76a70c0
RickonChen <2298847021@qq.com>
2019-01-31 18:37:25 +0800

    add pagination

--- project/base/views.py
+++ project/base/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from blog.models import Post
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home(request):
     items = Post.objects.all()
+    paginator = Paginator(items, 3)
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
     return render(request, 'base/home.html', { 'items': items, 'title': 'Home' })
