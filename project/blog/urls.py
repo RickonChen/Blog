14eb8c05a62c87fe6fe41ed48a703f37ec15a412
RickonChen <2298847021@qq.com>
2019-01-31 22:21:45 +0800

    add custom form for adding post

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<slug:slug>/', views.post, name='item'),
+    path('add/post/', views.add_post, name='add_post'),
+    path('edit/post/<int:pk>/', views.edit_post, name='edit_post'),
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
+from blog.forms import PostForm
 from blog.models import Post
 
 def post(request, slug=None):
     item = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post.html', {'item': item, 'title': item,})
+
+def add_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            # todo will store author here...
+            item.save()
+            form.save_m2m()
+            return redirect(item.get_absolute_url())
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form, 'title': 'Add post',})
+
+def edit_post(request, pk=None):
+    item = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST or None, request.FILES or None, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect(item.get_absolute_url())
+    else:
+        form = PostForm(instance=item)
+
+    title = 'Edit: %s' % item
+    return render(request, 'blog/post_form.html', { 'form': form, 'item': item, 'title': title })
